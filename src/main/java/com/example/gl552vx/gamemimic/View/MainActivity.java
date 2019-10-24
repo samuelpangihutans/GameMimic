b6c585a4a686ed77d1ebf378f58ab63408cef819
samuel sihombing <7316054@student.unpar.ac.id>
2019-10-24 11:30:48 +0700

    pada main activity tinggal periksa kembali activity ke 2 nya  namanya

--- src/main/java/com/example/gl552vx/gamemimic/View/MainActivity.java
+++ src/main/java/com/example/gl552vx/gamemimic/View/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.gl552vx.gamemimic.View;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.gl552vx.gamemimic.R;
 
@@ -11,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton();
+
+    }
+
+    private void startButton(){
+        Button startButton=(Button) findViewById(R.id.btn_start);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,CameraAct.class));
+            }
+        });
     }
 }
