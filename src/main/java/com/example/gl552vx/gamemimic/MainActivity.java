e3d7a91d87aebcd631558dc386e791a04713e991
samuel sihombing <7316054@student.unpar.ac.id>
2019-10-24 09:24:39 +0700

    first Commit App Android

---
+++ src/main/java/com/example/gl552vx/gamemimic/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gl552vx.gamemimic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
